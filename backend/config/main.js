a766c4046587bfc22a388dddc648d54a54e57628
Kaio Barbosa <kaio.barbosa0824@gmail.com>
2026-09-03 00:13:35 -0300

    first structure and desing at create_accoutn screen

--- backend/config/main.js
+++ backend/config/main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron/main')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 1280,
+    height: 730,
   })
 
   win.loadFile('frontend/public/index.html')
