296f515c3a38494b326d20529afd001342c1130c
Kaio Barbosa <kaio.barbosa0824@gmail.com>
2026-09-07 15:43:04 -0300

    add the verify password hash function. Not is implemented... but is created

--- backend/src/infrastructure/external_services/crypted_password.py
+++ backend/src/infrastructure/external_services/crypted_password.py
@@ -8,3 +8,9 @@ def hash_password(password):
     hashed_password = bcrypt.hashpw(password_bytes, salt)
 
     return hashed_password.decode("utf-8")
+
+def verify_password(password, hashed_password):
+    password_bytes = password.encode("utf-8")
+    hashed_password_bytes = hashed_password.encode("utf-8")
+
+    return bcrypt.checkpw(password_bytes, hashed_password_bytes)
