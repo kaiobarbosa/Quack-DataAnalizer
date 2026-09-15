d74b618558dbb47cbc19f6febdf6a8fa1a8fef0f
Kaio Barbosa <kaio.barbosa0824@gmail.com>
2026-09-14 21:35:54 -0300

    add the function that will be analize the type of user and derect the home_screen for active the correctly elements

--- frontend/src/services/home_services/home_screen.js
+++ frontend/src/services/home_services/home_screen.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
      
-    fetchDataEnterprise()
+    const userRole = getRoleFromToken();
+
+    if (userRole === 'pj') {
+        console.log("Usuário logado: Pessoa Jurídica (Empresa)");
+        fetchDataEnterprise();
+    }
 
 });
 
@@ -61,4 +66,29 @@ async function fetchDataEnterprise() {
         console.error('Erro de conexão:', error);
     }
 
+}
+
+async function getRoleFromToken() {
+    // 1. Pega o token salvo no localStorage
+    const token = localStorage.getItem('access_token');
+    if (!token) return null;
+
+    try {
+        // 2. O token tem o formato: header.payload.assinatura
+        // Pegamos a parte 1 (o payload, que é a segunda parte do array)
+        const payloadBase64 = token.split('.')[1];
+        
+        // 3. O JavaScript tem uma função nativa chamada atob() que decodifica Base64
+        const decodedJson = atob(payloadBase64);
+        
+        // 4. Transforma o JSON em um objeto JavaScript
+        const payload = JSON.parse(decodedJson);
+
+        // 5. Retorna o role que o backend Flask colocou lá!
+        return payload.role; // Vai retornar 'pj' ou 'pf'
+        
+    } catch (error) {
+        console.error("Erro ao decodificar o token:", error);
+        return null;
+    }
 }
