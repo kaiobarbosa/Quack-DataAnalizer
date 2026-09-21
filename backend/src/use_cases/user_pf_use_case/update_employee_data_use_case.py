9de59ffc8f171f69590a40fe4fa9d6dc11ef6485
Kaio Barbosa <kaio.barbosa0824@gmail.com>
2026-09-20 21:28:52 -0300

    solving the little bug into email validation in employee update

--- backend/src/use_cases/user_pf_use_case/update_employee_data_use_case.py
+++ backend/src/use_cases/user_pf_use_case/update_employee_data_use_case.py
@@ -6,7 +6,7 @@ def update_employee_data_use_case(email, data):
 
     data_entity = entity_employee_update(data)
 
-    if validate_email(data_entity['email']) == False:
+    if validate_email(data['email']) == False:
         return {"message": "Invalid email format"}, 400
 
     if update_employee_repository(data_entity, email) == False:
